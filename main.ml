e93bd09ea6c335a303b5f2987e634180f2d21d80
greeghost <hugo.boulier@ens-rennes.fr>
2021-11-07 14:52:21 +0100

    miscellaneous changes

--- main.ml
+++ main.ml
@@ -17,7 +17,7 @@ let _ =
     let ir = Codegen.ir_of_ast ast in
     print_endline (Llvm.string_of_ir ir)
 
-    
+
   with
     Lexer.Unexpected_character e ->
     Printf.printf "Unexpected character: `%c' at position '%d' on line '%d'\n"
